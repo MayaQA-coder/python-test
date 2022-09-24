1193e0abc3ea7ebb2838541530bdef0a43b190ba
MayaQA-coder <maya.gaievska@outlook.com>
2022-09-23 17:09:48 -0700

    Second change

--- program1.py
+++ program1.py
@@ -2,4 +2,20 @@ def func1():
     print("Hi")
     print("Hey from Martin")
 
+
 func1()
+
+
+def func2(answer):
+    if answer < 5:
+        return "After all I done for you!"
+    elif answer > 10:
+        return "OMG!!!! You are the best and I love you to the moon and back!"
+    else:
+        return "I love you too, my Kotik!"
+
+
+user_input = input("How much do you love Maya, from 1 to 10?\n")
+answer = int(user_input)
+
+print(func2(answer))
